495d0e50efcceade42662b01ae96b9610acf04a6
Kaban <kopylovdk@gmail.com>
2021-07-08 20:57:14 +0300

    bug fix

--- testproject/api/urls.py
+++ testproject/api/urls.py
@@ -13,12 +13,13 @@ urlpatterns = [
 
     path('polls/create/', views.PollsAPIView.as_view()),
     path('polls/update/<int:pk>', views.PollsAPIView.as_view()),           #
+
     path('polls/list/', views.PollsAPIView.as_view()),
-    path('polls/activeList/', views.PollsActiveListAPIView.as_view()),
+    path('polls/allActive/', views.PollsListAPIView.as_view()),
     path('polls/<int:pk>/', views.PollsOneAPIView.as_view()),             #
 
     path('questions/', views.QuestionAPIView.as_view()),
     path('questions/<int:pk>', views.QuestionAPIView.as_view()),     #
 
-    path('questionOptions/', views.QuestionOptionsAPIView.as_view()),
+    path('questionOptions/<int:pk>', views.QuestionOptionsAPIView.as_view()),
 ]
